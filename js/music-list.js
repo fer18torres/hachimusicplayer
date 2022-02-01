12186f4574c644c2a749ccaf565689a09f3f90a4
fer18torres <79620178+fer18torres@users.noreply.github.com>
2022-01-31 21:23:36 -0300

    subida de archivos

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+
+    {
+        name: "Goodbye",
+        artist: "Apparat",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Fluorecent Adolescent",
+        artist: "Arctic Monkeys",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Smokin Out The Window",
+        artist: "Bruno Mars Anderson - Paak Silk Sonic",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Lost In Your Love",
+        artist: "Friendzone ft KARRA",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Save Your Tears Remix",
+        artist: "The Weeknd - Ariana Grande",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Danger",
+        artist: "Wubbaduck",
+        img: "music-6",
+        src: "music-6"
+    },
+    
+    ];
